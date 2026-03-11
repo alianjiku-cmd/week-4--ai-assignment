0966101f437bc3d8147e67132c0f5de0c918632f
alianjiku-cmd <alianjiku@gmail.com>
2026-03-11 12:08:52 -0700

    Enhance README with data definitions and AI concepts
    Added detailed explanations for data, its categories, and AI learning.

--- READ-me assignment.py
+++ READ-me assignment.py
@@ -2,12 +2,49 @@
 
 Name: Alice Wanjiku
 
-## Question 1
-Your answer here
+## Question 1.
 
+"Data"
+facts and statistics collected together for reference or analysis. 
+This includes
+1.facts
+2. figures
+3. statistics
+4. details
+5. particulars
+6. specifics
+7. features
+the quantities, characters, or symbols on which operations are performed by a computer, being stored and transmitted in the form of electrical signals and recorded on magnetic, optical, or mechanical recording media.
+philosophy
+things known or assumed as facts, making the basis of reasoning or calculation."facts and statistics collected together for reference or analysis. See also datum.
+"there is very little data available"
+Similar:
+facts
+figures
+statistics
+details
+particulars
+specifics
+features
+the quantities, characters, or symbols on which operations are performed by a computer, being stored and transmitted in the form of electrical signals and recorded on magnetic, optical, or mechanical recording media.
+philosophy
+things known or assumed as facts, making the basis of reasoning or calculation.
+Data isfacts and statistics collected together for reference or analysis. 
+Data ca be categolised into
+-facts
+-figures
+-statistics
+-details
+-particularsspecifics
+features
+the quantities, characters, or symbols on which operations are performed by a computer, being stored and transmitted in the form of electrical signals and recorded on magnetic, optical, or mechanical recording media.
+philosophy
+things known or assumed as facts, making the basis of reasoning or calculation.
 ## Question 2
-Your answer here
+Structured vs unstructured data
 
 ## Question 3
-Your answer here
+How AI learns from data
 
+## Question 4
+Bias in datasets
