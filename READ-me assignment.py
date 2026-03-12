f8b698e086b8d335edb0694cebe08560bf1ede91
alianjiku-cmd <alianjiku@gmail.com>
2026-03-11 21:19:17 -0700

    Update READ-me assignment.py

--- READ-me assignment.py
+++ READ-me assignment.py
@@ -5,41 +5,9 @@ Name: Alice Wanjiku
 ## Question 1.
 
 "Data"
-facts and statistics collected together for reference or analysis. 
-This includes
-1.facts
-2. figures
-3. statistics
-4. details
-5. particulars
-6. specifics
-7. features
-the quantities, characters, or symbols on which operations are performed by a computer, being stored and transmitted in the form of electrical signals and recorded on magnetic, optical, or mechanical recording media.
-philosophy
-things known or assumed as facts, making the basis of reasoning or calculation."facts and statistics collected together for reference or analysis. See also datum.
-"there is very little data available"
-Similar:
-facts
-figures
-statistics
-details
-particulars
-specifics
-features
-the quantities, characters, or symbols on which operations are performed by a computer, being stored and transmitted in the form of electrical signals and recorded on magnetic, optical, or mechanical recording media.
-philosophy
-things known or assumed as facts, making the basis of reasoning or calculation.
-Data isfacts and statistics collected together for reference or analysis. 
-Data ca be categolised into
--facts
--figures
--statistics
--details
--particularsspecifics
-features
-the quantities, characters, or symbols on which operations are performed by a computer, being stored and transmitted in the form of electrical signals and recorded on magnetic, optical, or mechanical recording media.
-philosophy
-things known or assumed as facts, making the basis of reasoning or calculation.
+Defination
+
+
 ## Question 2
 Structured vs unstructured data
 
