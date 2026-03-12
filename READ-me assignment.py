314de1b14895d413dd984e6fcf378bd7225890cf
alianjiku-cmd <alianjiku@gmail.com>
2026-03-11 21:41:29 -0700

    Update READ-me assignment.py

--- READ-me assignment.py
+++ READ-me assignment.py
@@ -10,7 +10,23 @@ Data is a raw unorganised fact, symbols or observations that can be prossed meas
 
 
 ## Question 2
-Structured vs unstructured data
+"Structured vs unstructured data"
+Structured” and “unstructured” are terms used to classify data based on its format and schema rules or lack therefo.
+
+"Structured data"
+Structured data has a fixed schema and fits neatly into rows and columns, such as names and phone numbers
+
+"Unstructured data"
+Unstructured data has no fixed schema and can have a more complex format, such as audio files and web pages.
+
+"key areas where structured and unstructured data differ"Format: Structured data has a strict, predefined data model. Unstructured data does not have a predefined format.
+
+Storage: Structured data storage systems have rigid schemas, such as those in relational databases or data warehouses. Unstructured data is often stored in its native format in nonrelational databases or data lakes.
+
+Use cases: Organizations can use both structured and unstructured data across artificial intelligence (AI) and analytics use cases. Structured data is often used in machine learning (ML) and drives ML algorithms. Unstructured data is often used in natural language processing (NLP) and is a rich and diverse data source for generative AI (gen AI) models.
+
+Complexity: Structured data is easier to manipulate and analyze for general business users with traditional tools. Unstructured data can be more complex and requires specialized skills and tools to parse and analyze.
+
 
 ## Question 3
 How AI learns from data
