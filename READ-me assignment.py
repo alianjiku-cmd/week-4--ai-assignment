76af2860700ca229415dc008af02fb8a1679dd9f
alianjiku-cmd <alianjiku@gmail.com>
2026-03-12 03:17:41 -0700

    Update READ-me assignment.py

--- READ-me assignment.py
+++ READ-me assignment.py
@@ -4,25 +4,28 @@ Name: Alice Wanjiku
 
 ## Question 1.
 
-"Data"
+**Data**
 Defination:
 Data is a raw unorganised fact, symbols or observations that can be prossed measured and analyzed to create information for decision-making.
 
 
 ## Question 2
-"Structured vs unstructured data"
+**Structured vs unstructured data**
 Structured” and “unstructured” are terms used to classify data based on its format and schema rules or lack therefo.
 
-"Structured data"
+
+   **Structured data**
+
 -Structured data has a fixed schema and fits neatly into rows and columns, such as names and phone numbers. It is organized in a clear, predefined format.
 -The standardized nature of structured data makes it easily decipherable by data analytics tools, machine learning algorithms and human users.Structured data can include both quantitative data (such as prices or revenue figures) and qualitative data (such as dates, names, addresses and credit card numbers).
   For example, a financial report with company names, expense values and reporting periods organized into rows and columns is considered structured data.
 
 
-"Unstructured data"
+**Unstructured data**
 Unstructured data has no fixed schema and can have a more complex format, such as audio files and web pages.
 
-"key areas where structured and unstructured data differ" 
+**key areas where structured and unstructured data differ**
+  
 1. Format:Structured data has a strict, predefined data model. Unstructured data does not have a predefined format.
 
 2. Storage: Structured data storage systems have rigid schemas, such as those in relational databases or data warehouses. Unstructured data is often stored in its native format in nonrelational databases or data lakes.
@@ -35,10 +38,10 @@ Unstructured data has no fixed schema and can have a more complex format, such a
                 Unstructured data can be more complex and requires specialized skills and tools to parse and analyze.
 
                      
-"The benefits of structured data."
+**The benefits of structured data.**
   
 . Works well with machine learning: Machine learning can process both structured and unstructured data. However, it is easier for ML applications to analyze and 
-                  draw insights from structured data due to its specific and organized architecture.
+                                   draw insights from structured data due to its specific and organized architecture.
 
 . Accessible and easy to use: Understanding structured data does not require in-depth data science knowledge. 
                               Due to its standard format and high level of organization, most users find it easy to access and interpret structured data.
@@ -50,9 +53,10 @@ Unstructured data has no fixed schema and can have a more complex format, such a
 . Limited usage: Structured data has a predefined data model that can only be used for its intended purpose, which limits its flexibility and usability.
                  Mining more insights requires modifications or additional data.
 
-. Limited storage options: Structured data storage repositories typically have rigid schemas, such as those within a relational database or data warehouse. Changes to data requirements need updating all structured data, which is time and resource-intensive.
+. Limited storage options: Structured data storage repositories typically have rigid schemas, such as those within a relational database or data warehouse.
+  Changes to data requirements need updating all structured data, which is time and resource-intensive.
 
-"Use cases for structured data"
+**Use cases for structured data~~**
 (a) AI model training 
 (b) Customer relationship management (CRM)
 (c) Business intelligence (BI)
@@ -60,8 +64,13 @@ Unstructured data has no fixed schema and can have a more complex format, such a
 (e) Search engine optimization (SEO) rich snippets
 
 
-## Question 3
-How AI learns from data
+   **## Question 3**
+
+**How AI learns from data~~**
+
+AI learns from data by using algorithms to analyze large datasets, identifying patterns, 
+and adjusting internal parameters (weights) to improve predictive accuracy. 
+Through machine learning techniques, AI transforms data into numerical matrices to recognize relationships.
 
 ## Question 4
 Bias in datasets
